db3315bef6db67c80e1a56eee11fbe7825e33351
Aditya J <ajain15@uic.edu>
2020-04-12 00:47:34 -0400

    history mode middleware

--- server.js
+++ server.js
@@ -5,10 +5,11 @@ const game = new Game()
 let port = process.env.PORT || 8000;
 let express = require('express');
 let app = express();
+const history = require('connect-history-api-fallback')
 let server = require('http').createServer(app).listen(port, function () {
   console.log('Server listening at port: ', port);
 });
-
+app.use(history)
 // Tell server where to look for files
 app.use(express.static('public'))
 
